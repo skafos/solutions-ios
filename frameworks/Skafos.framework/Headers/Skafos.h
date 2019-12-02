c167b62454c0701c2852541bb07098cf85b2d0e9
Distiller <distiller@static.162.252.208.40.cyberlynk.net>
2019-12-02 20:36:46 +0000

    Release v5.2.1

--- frameworks/Skafos.framework/Headers/Skafos.h
+++ frameworks/Skafos.framework/Headers/Skafos.h
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT const unsigned char SkafosVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Skafos/PublicHeader.h>
 
+#import "zip.h"
+#import "unzip.h"
---
+++ frameworks/SkafosVTO.framework/Headers/SkafosVTO.h
@@ -0,0 +1,19 @@
+//
+//  SkafosVTO.h
+//  SkafosVTO
+//
+//  Created by Ian Terrell on 11/22/19.
+//  Copyright © 2019 Skafos.ai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SkafosVTO.
+FOUNDATION_EXPORT double SkafosVTOVersionNumber;
+
+//! Project version string for SkafosVTO.
+FOUNDATION_EXPORT const unsigned char SkafosVTOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkafosVTO/PublicHeader.h>
+
+
